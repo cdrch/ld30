4ec5cd20481f9b1b0d89e9e8931215a9c5db39ac
Carter David Henning <carterdavidhenning@yahoo.com>
2014-08-24 03:16:15 +0000

    The lighting is working and fairly awesome.

--- Game.js
+++ Game.js
@@ -95,6 +95,7 @@ BalanceGame.Game.prototype = {
 	 //this.game.physics.arcade.collide(this.player, this.doors);
     
     this.movePlayer();
+    this.updateLightning();
     
     if (BalanceGame.playerInfo.health === 0)
     {
@@ -553,71 +554,179 @@ BalanceGame.Game.prototype = {
 	// MAGIC FUNCTIONS
 	
 	setupMagic: function () {
-	 // // Lightning
+	  // Lightning
 	  
-  //   this.explosionGroup = this.game.add.group();
+    this.explosionGroup = this.game.add.group();
 
-  //   // Create a bitmap for the lightning bolt texture
-  //   this.lightningBitmap = this.game.add.bitmapData(200, 1000);
+    // Create a bitmap for the lightning bolt texture
+    this.lightningBitmap = this.game.add.bitmapData(200, 1000);
 
-  //   // Create a sprite to hold the lightning bolt texture
-  //   this.lightning = this.game.add.image(this.game.width/2, 80, this.lightningBitmap);
+    // Create a sprite to hold the lightning bolt texture
+    // this.lightning = this.game.add.image(this.game.width/2, 80, this.lightningBitmap);
+    this.lightning = this.game.add.image(this.player.x, this.player.y, this.lightningBitmap);
 
-  //   // Browser must support WebGL
-  //   this.lightning.filters = [ this.game.add.filter('Glow') ];
-  //   this.lightning.anchor.setTo(0.5, 0);
+    // Browser must support WebGL
+    this.lightning.filters = [ this.game.add.filter('Glow') ];
+    this.lightning.anchor.setTo(0.5, 0);
 
-  //   // Create a white rectangle that we'll use to represent the flash
-  //   this.flash = this.game.add.graphics(0, 0);
-  //   this.flash.beginFill(0xffffff, 1);
-  //   this.flash.drawRect(0, 0, this.game.width, this.game.height);
-  //   this.flash.endFill();
-  //   this.flash.alpha = 0;
+    // Create a white rectangle that we'll use to represent the flash
+    this.flash = this.game.add.graphics(0, 0);
+    this.flash.beginFill(0xffffff, 1);
+    this.flash.drawRect(0, 0, this.game.width, this.game.height);
+    this.flash.endFill();
+    this.flash.alpha = 0;
+    
+    this.nextBoltAt = 0;
+    this.shotDelay = 3000;
+    this.boltReady = true;
+	},
+	
+	updateLightning: function () {
+	  if (this.boltReady === true)
+	  {
+	    this.lightning.x = this.player.x + this.player.width/2;
+	    this.lightning.y = this.player.y + this.player.height/2;
+	  }
+	  else
+	  {
+	    // do nothing
+	  }
+	},
+	
+	resetLightningTimer: function () {
+	  this.boltReady = true;
 	},
 	
 	summonLightning: function () {
-	 // // Rotate the lightning sprite so it goes in the
-  //       // direction of the pointer
-  //       this.lightning.rotation =
-  //           this.game.math.angleBetween(
-  //               this.lightning.x, this.lightning.y,
-  //               this.game.input.activePointer.x, this.game.input.activePointer.y
-  //           ) - Math.PI/2;
+	  if(this.boltReady === false)
+	  {
+	    return;
+	  }
+	  
+	 // this.nextBoltAt = this.time.now + this.shotDelay;
+	  
+	  this.time.events.add(3000, this.resetLightningTimer, this);
+	  // Rotate the lightning sprite so it goes in the
+    // direction of the pointer
+    this.lightning.rotation =
+        this.game.math.angleBetween(
+            this.lightning.x, this.lightning.y,
+            this.game.input.activePointer.x, this.game.input.activePointer.y
+        ) - Math.PI/2;
 
-  //       // Calculate the distance from the lightning source to the pointer
-  //       var distance = this.game.math.distance(
-  //               this.lightning.x, this.lightning.y,
-  //               this.game.input.activePointer.x, this.game.input.activePointer.y
-  //           );
+    // Calculate the distance from the lightning source to the pointer
+    var distance = this.game.math.distance(
+            this.lightning.x, this.lightning.y,
+            this.game.input.activePointer.x, this.game.input.activePointer.y
+        );
 
-  //       // Create the lightning texture
-  //       this.createLightningTexture(this.lightningBitmap.width/2, 0, 20, 3, false, distance);
+    // Create the lightning texture
+    this.createLightningTexture(this.lightningBitmap.width/2, 0, 20, 3, false, distance);
 
-  //       // Make the lightning sprite visible
-  //       this.lightning.alpha = 1;
+    // Make the lightning sprite visible
+    this.lightning.alpha = 1;
 
-  //       // Fade out the lightning sprite using a tween on the alpha property.
-  //       // Check out the "Easing function" examples for more info.
-  //       this.game.add.tween(this.lightning)
-  //           .to({ alpha: 0.5 }, 100, Phaser.Easing.Bounce.Out)
-  //           .to({ alpha: 1.0 }, 100, Phaser.Easing.Bounce.Out)
-  //           .to({ alpha: 0.5 }, 100, Phaser.Easing.Bounce.Out)
-  //           .to({ alpha: 1.0 }, 100, Phaser.Easing.Bounce.Out)
-  //           .to({ alpha: 0 }, 250, Phaser.Easing.Cubic.In)
-  //           .start();
+    // Fade out the lightning sprite using a tween on the alpha property.
+    // Check out the "Easing function" examples for more info.
+    this.game.add.tween(this.lightning)
+        .to({ alpha: 0.5 }, 100, Phaser.Easing.Bounce.Out)
+        .to({ alpha: 1.0 }, 100, Phaser.Easing.Bounce.Out)
+        .to({ alpha: 0.5 }, 100, Phaser.Easing.Bounce.Out)
+        .to({ alpha: 1.0 }, 100, Phaser.Easing.Bounce.Out)
+        .to({ alpha: 0 }, 250, Phaser.Easing.Cubic.In)
+        .start();
 
-  //       // Create the flash
-  //       this.flash.alpha = 1;
-  //       this.game.add.tween(this.flash)
-  //           .to({ alpha: 0 }, 100, Phaser.Easing.Cubic.In)
-  //           .start();
+    // Create the flash
+    this.flash.alpha = 1;
+    this.game.add.tween(this.flash)
+        .to({ alpha: 0 }, 100, Phaser.Easing.Cubic.In)
+        .start();
 
-  //       // Shake the camera by moving it up and down 5 times really fast
-  //       this.game.camera.y = 0;
-  //       this.game.add.tween(this.game.camera)
-  //           .to({ y: -10 }, 40, Phaser.Easing.Sinusoidal.InOut, false, 0, 5, true)
-  //           .start();
-  //   }
+    // Shake the camera by moving it up and down 5 times really fast
+    // this.game.camera.y = 0;
+    // this.game.add.tween(this.game.camera)
+    //     .to({ y: -10 }, 40, Phaser.Easing.Sinusoidal.InOut, false, 0, 5, true)
+    //     .start();
+	  this.boltReady = false;
+	},
+	
+	getExplosion: function () {
+	  
+	},
+	
+	createLightningTexture: function (x, y, segments, boltWidth, branch, distance) {
+	  // Get the canvas drawing context for the lightningBitmap
+    var ctx = this.lightningBitmap.context;
+    var width = this.lightningBitmap.width;
+    var height = this.lightningBitmap.height;
+
+    // Our lightning will be made up of several line segments starting at
+    // the center of the top edge of the bitmap and ending at the target.
+
+    // Clear the canvas
+    if (!branch) ctx.clearRect(0, 0, width, height);
+
+    // Draw each of the segments
+    for(var i = 0; i < segments; i++) {
+        // Set the lightning color and bolt width
+        ctx.strokeStyle = 'rgb(255, 255, 255)';
+        ctx.lineWidth = boltWidth;
+
+        ctx.beginPath();
+        ctx.moveTo(x, y);
+
+        // Calculate an x offset from the end of the last line segment and
+        // keep it within the bounds of the bitmap
+        if (branch) {
+            // For a branch
+            x += this.game.rnd.integerInRange(-10, 10);
+        } else {
+            // For the main bolt
+            x += this.game.rnd.integerInRange(-30, 30);
+        }
+        if (x <= 10) x = 10;
+        if (x >= width-10) x = width-10;
+
+        // Calculate a y offset from the end of the last line segment.
+        // When we've reached the target or there are no more segments left,
+        // set the y position to the distance to the target. For branches, we
+        // don't care if they reach the target so don't set the last coordinate
+        // to the target if it's hanging in the air.
+        if (branch) {
+            // For a branch
+            y += this.game.rnd.integerInRange(10, 20);
+        } else {
+            // For the main bolt
+            y += this.game.rnd.integerInRange(20, distance/segments);
+        }
+        if ((!branch && i == segments - 1) || y > distance) {
+            // This causes the bolt to always terminate at the center
+            // lightning bolt bounding box at the correct distance to
+            // the target. Because of the way the lightning sprite is
+            // rotated, this causes this point to be exactly where the
+            // player clicked or tapped.
+            y = distance;
+            if (!branch) x = width/2;
+        }
+
+        // Draw the line segment
+        ctx.lineTo(x, y);
+        ctx.stroke();
+
+        // Quit when we've reached the target
+        if (y >= distance) break;
+
+        // Draw a branch 20% of the time off the main bolt only
+        if (!branch) {
+            if (this.game.math.chanceRoll(20)) {
+                // Draws another, thinner, bolt starting from this position
+                this.createLightningTexture(x, y, 10, 1, true, distance);
+            }
+        }
+    }
+
+    // This just tells the engine it should update the texture cache
+    this.lightningBitmap.dirty = true;
 	},
 	
 	// WEATHER FUNCTIONS
@@ -655,3 +764,35 @@ BalanceGame.Game.prototype = {
 	}
 
 };
+
+Phaser.Filter.Glow = function (game) {
+    Phaser.Filter.call(this, game);
+
+    this.fragmentSrc = [
+        "precision lowp float;",
+        "varying vec2 vTextureCoord;",
+        "varying vec4 vColor;",
+        'uniform sampler2D uSampler;',
+
+        'void main() {',
+            'vec4 sum = vec4(0);',
+            'vec2 texcoord = vTextureCoord;',
+            'for(int xx = -4; xx <= 4; xx++) {',
+                'for(int yy = -3; yy <= 3; yy++) {',
+                    'float dist = sqrt(float(xx*xx) + float(yy*yy));',
+                    'float factor = 0.0;',
+                    'if (dist == 0.0) {',
+                        'factor = 2.0;',
+                    '} else {',
+                        'factor = 2.0/abs(float(dist));',
+                    '}',
+                    'sum += texture2D(uSampler, texcoord + vec2(xx, yy) * 0.002) * factor;',
+                '}',
+            '}',
+            'gl_FragColor = sum * 0.025 + texture2D(uSampler, texcoord);',
+        '}'
+    ];
+};
+
+Phaser.Filter.Glow.prototype = Object.create(Phaser.Filter.prototype);
+Phaser.Filter.Glow.prototype.constructor = Phaser.Filter.Glow;
